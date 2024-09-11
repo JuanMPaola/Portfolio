7b85643cebc9d61025d76c88c99d45cfa63d4e39
JuanMPaola <juanmpaola25@gmail.com>
2024-09-11 15:24:41 -0300

    home 50% starting cards

---
+++ src/app/projects/projects.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ProjectCardComponent } from "../project-card/project-card.component";
+
+@Component({
+  selector: 'app-projects',
+  standalone: true,
+  imports: [ProjectCardComponent],
+  templateUrl: './projects.component.html',
+  styleUrl: './projects.component.css'
+})
+export class ProjectsComponent {
+
+  constructor(private titleService: Title){
+    this.titleService.setTitle('Juan Manuel Paola - Projects');
+  }
+}
